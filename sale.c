25457fcc44027b975773f2ef13ef7dc588364849
Ch1nath1p <139220960+Ch1nath1p@users.noreply.github.com>
2023-12-06 12:13:39 +0700

    Update sale.cpp

--- sale.c
+++ sale.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+/* 3 */
 int main(){
 	float p1,p2,p3;
 	printf("Product_1_piece\n");
@@ -10,4 +11,27 @@ int main(){
 	printf("Product_3_piece\n");
 	scanf("%f %f %f",&p1,&p2,&p3);
 	printf("Discount = %f",p1+p2+p3-((p1+p2+p3)*0.30));
+}
+
+/* 4 */
+int main(){
+	float p1,P1,p2,P2,p3,P3,vat1,vat2,vat3;
+	printf("Product_1_piece\n");
+	scanf("%f",&p1);
+	P1 = p1-(0.05*p1);
+	printf("Discount = %f\n",P1);
+	vat1 = P1*0.07;
+	printf("vat = %f\n",P1+vat1);
+	printf("Product_2_piece\n");
+	scanf("%f %f",&p1,&p2);
+	P2 = p1+p2-((p1+p2)*0.15);
+	printf("Discount = %f\n",P2);
+	vat2 = P2*0.07;
+	printf("vat = %f\n",P2+vat2);
+	printf("Product_3_piece\n");
+	scanf("%f %f %f",&p1,&p2,&p3);
+	P3 = p1+p2+p3-((p1+p2+p3)*0.30);
+	printf("Discount = %f\n",P3);
+	vat3 = P3*0.07;
+	printf("vat = %f\n",P3+vat3);
 }
